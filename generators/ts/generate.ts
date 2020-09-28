25d1d5263f02fa72f6333c11fdfa520d67a14e67
kkm <kubasurdej@gmail.com>
2020-09-28 09:57:18 +0200

    Modify file structure, add main index file

--- generators/ts/generate.ts
+++ generators/ts/generate.ts
@@ -1,27 +1,19 @@
 import { join } from "path";
-import { execute } from "../utils/shellUtils";
-import { replaceInManyFiles, findFiles } from "../utils/fileUtils";
+import { execute } from "../../utils/shellUtils";
+import { replaceInManyFiles, findFiles } from "../../utils/fileUtils";
 
-const main = async () => {
-  const openapi = join(__dirname, "openapi.json");
+export const generateTSClient = async (openapiFile: string): Promise<void> => {
   const config = join(__dirname, "config.yaml");
   const generator = "typescript-axios";
 
   await execute(
-    `openapi-generator generate -i ${openapi} -c ${config} -g ${generator}`
+    `openapi-generator generate -i ${openapiFile} -c ${config} -g ${generator}`
   );
-
   await execute("yarn add -D typedoc@0.19.2 typedoc-plugin-markdown@3.0.3");
 
   const typescriptFiles = await findFiles("ts");
-
   await replaceInManyFiles(typescriptFiles, "AnyType", "any");
 
   await execute("yarn typedoc --plugin typedoc-plugin-markdown");
   await execute("yarn build");
 };
-
-main().catch((e) => {
-  console.error(`Error when generating client: ${e}`);
-  process.exit(1);
-});
---
+++ index.ts
@@ -0,0 +1,4 @@
+import { generateTSClient } from "./generators/ts/generate";
+import { join } from "path";
+
+generateTSClient(join(__dirname, "openapi.json")).catch((e) => console.log(e));
