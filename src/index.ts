dcb50e7054a4a73d0fd527ea619044a06f11cd9e
kkm <kubasurdej@gmail.com>
2020-10-02 21:01:39 +0200

    Fix ts tests location

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,12 @@ import { runTSWorkflow } from "./workflows/ts";
 
 const clientsLocation = join(__dirname, "..", "clients");
 const openapiLocation = join(__dirname, "..", "openapi.json");
+const testsLocation = join(__dirname, "..", "tests");
 
 const runAllWorkflows = async () => {
-  return Promise.all([runTSWorkflow(clientsLocation, openapiLocation)]);
+  return Promise.all([
+    runTSWorkflow(clientsLocation, openapiLocation, testsLocation),
+  ]);
 };
 
 runAllWorkflows().catch((e) => {
--- src/workflows/ts.ts
+++ src/workflows/ts.ts
@@ -4,9 +4,11 @@ import { execute } from "../utils/shellUtils";
 
 export const runTSWorkflow = async (
   clientsLocation: string,
-  openapiLocation: string
+  openapiLocation: string,
+  testsLocation: string
 ): Promise<void> => {
   const tsClientLocation = join(clientsLocation, "ts");
+  const tsTestsLocation = join(testsLocation, "ts");
   await generateTSClient(openapiLocation, tsClientLocation);
-  await execute("/bin/bash ./execute.sh", tsClientLocation);
+  await execute("/bin/bash ./execute.sh", tsTestsLocation);
 };
