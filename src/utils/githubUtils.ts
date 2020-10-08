cb4ddd489beaa191b9d30ce396316662f66e60a8
kkm <kubasurdej@gmail.com>
2020-10-08 18:00:52 +0200

    Fetch gihub actor from environment variable

--- src/utils/githubUtils.ts
+++ src/utils/githubUtils.ts
@@ -2,15 +2,14 @@ import { execute } from "./shellUtils";
 
 export interface PushRepositoryInput {
   repository: string;
-  actor: string;
   token: string;
 }
 
 export const pushRepository = async ({
-  actor,
   token,
   repository,
 }: PushRepositoryInput) => {
+  const actor = process.env.GITHUB_ACTOR;
   execute(
     `git push https://${actor}:${token}@github.com/${repository}.git" HEAD:master`
   );
