1a0a0510518e1c8d80bd56748132f9028a5937fe
kkm <kubasurdej@gmail.com>
2020-10-13 18:14:39 +0200

    Add @helperone/credentials library to allow to perform e2e tests

--- tests/ts/tests/library.test.ts
+++ tests/ts/tests/library.test.ts
@@ -1,7 +1,15 @@
 import { Configuration } from "hyperone";
+import { getPassportCredentialsHelper } from "@hyperone/credentials"
 
 describe("library", () => {
   it("allows to create new configuration object", async () => {
     expect(() => new Configuration()).not.toThrow();
   });
+
+  it("allows to perform request using credentials helper", async () => {
+    const helper = getPassportCredentialsHelper()
+    const config = new Configuration(
+      {accessToken: helper.getToken("https://api.hyperone.com/v2")}
+    )
+  })
 });
