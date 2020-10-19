d625c2c4ed604ab003262fbce08fa0df9808eb92
kkm <kubasurdej@gmail.com>
2020-10-19 18:21:03 +0200

    Add second test to go client

--- tests/go/_api_test.go
+++ tests/go/_api_test.go
@@ -19,3 +19,20 @@ func TestPassportContext(t *testing.T) {
 		t.Errorf("Status code different than 200: %d", response.StatusCode)
 	}
 }
+
+func TestPassportContextWithError(t *testing.T) {
+	cfg := NewConfiguration()
+	c := NewAPIClient(cfg)
+	p, err := GetPassportContextProvider("")
+	require.NoError(t, err)
+
+	ctx, err := p.CtxWithError()
+	require.NoError(t, err)
+
+	_, response, err := c.IamProjectApi.IamProjectList(ctx, nil)
+	require.NoError(t, err)
+
+	if response.StatusCode != 200 {
+		t.Errorf("Status code different than 200: %d", response.StatusCode)
+	}
+}
