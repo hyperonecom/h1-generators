ad54079779df5e640c7857079c40a3f3402bf1b5
kkm <kubasurdej@gmail.com>
2020-10-26 22:57:43 +0100

    Add test for jwt used as api key

--- tests/python/test.py
+++ tests/python/test.py
@@ -2,10 +2,27 @@ from credentials import get_passport_credentials_helper
 from h1 import Configuration, ApiClient, IamProjectApi
 
 
-provider = get_passport_credentials_helper()
-cfg = Configuration()
-cfg.access_token = provider.get_token("https://api.hyperone.com/v2")
+def test_access_token_configuration():
+    provider = get_passport_credentials_helper()
+    cfg = Configuration()
+    cfg.access_token = provider.get_token("https://api.hyperone.com/v2")
 
-api_client = ApiClient(cfg)
-project_api = IamProjectApi(api_client)
-project_api.iam_project_list()
+    api_client = ApiClient(cfg)
+    project_api = IamProjectApi(api_client)
+    project_api.iam_project_list()
+
+
+def test_api_key_configuration():
+    audience = "https://api.hyperone.com/v2"
+    provider = get_passport_credentials_helper()
+    cfg = Configuration(api_key=provider.get_token(
+        audience), api_key_prefix="Bearer")
+    cfg.refresh_api_key_hook = lambda x: provider.get_token(audience)
+
+    api_client = ApiClient(cfg)
+    project_api = IamProjectApi(api_client)
+    project_api.iam_project_list()
+
+
+test_access_token_configuration()
+test_api_key_configuration()
