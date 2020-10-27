e1c1bc0a2c643f922ceda3608fc1ea1a7f0a5bd9
kkm <kubasurdej@gmail.com>
2020-10-27 23:11:12 +0100

    Use super() method in python configuration

--- tests/python/test.py
+++ tests/python/test.py
@@ -14,8 +14,8 @@ def test_access_token_configuration():
 
 def test_api_key_configuration():
     provider = get_passport_credentials_helper()
-    cfg = Configuration()
-    cfg.access_token_function = lambda self: provider.get_token(
+    cfg = Configuration(host="http://localhost:8080")
+    cfg.access_token_function = lambda: provider.get_token(
         "https://api.hyperone.com/v2")
 
     api_client = ApiClient(cfg)
