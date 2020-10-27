57a3ee7f186775bce70fd16a8e6d4450ea897b21
kkm <kubasurdej@gmail.com>
2020-10-28 00:51:59 +0100

    Fix getting access token in python client

--- src/generators/python/_configuration.py
+++ src/generators/python/_configuration.py
@@ -2,10 +2,23 @@ from .configuration_base import Configuration as BaseConfig
 
 
 class Configuration(BaseConfig):
+    def __init__(self, host="https://api.hyperone.com/v2",
+                 api_key=None, api_key_prefix=None,
+                 username=None, password=None,
+                 discard_unknown_keys=False,
+                 ):
+        self.access_token_function = None
+        super().__init__(host=host,
+                         api_key=api_key, api_key_prefix=api_key_prefix,
+                         username=username, password=password,
+                         discard_unknown_keys=discard_unknown_keys,
+                         )
+
     def get_access_token(self):
-        access_token_function = getattr(self, "access_token_function")
-        if callable(access_token_function):
-            return access_token_function()
+        if self.access_token_function is not None:
+            access_token_function = getattr(self, "access_token_function")
+            if callable(access_token_function):
+                return access_token_function()
 
         if self.access_token is not None:
             return self.access_token
