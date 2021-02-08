e65bd521c85ea754caff82ef3b8900b38a96e1ed
Adam Dobrawy <naczelnik@jawnosc.tk>
2021-02-08 01:39:30 +0100

    Incrase stderr buffer size for output

--- src/utils/shellUtils.ts
+++ src/utils/shellUtils.ts
@@ -1,8 +1,11 @@
 import { exec, ExecOptions } from "child_process";
 
+const TEN_MEGA_BYTE = 1024 * 1024 * 10;
+
 export const execute = async (cmd: string, cwd?: string) => {
   const options: ExecOptions = {};
   options.cwd = cwd;
+  options.maxBuffer = TEN_MEGA_BYTE;
 
   return new Promise((resolve, reject) => {
     exec(cmd, options, (err, stdout, stderr) => {
